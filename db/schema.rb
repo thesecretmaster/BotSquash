916bdcb6d45c1ccfd68de83c7b1b2d59474a25ec
ArtOfCode- <hello@artofcode.co.uk>
2018-07-08 22:25:06 +0100

    Schema for Devise/Rolify

--- db/schema.rb
+++ db/schema.rb
@@ -10,62 +10,100 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_180_708_195_543) do
-  create_table 'actions', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci', force: :cascade do |t|
-    t.bigint 'bot_id'
-    t.string 'actionable_type'
-    t.bigint 'actionable_id'
-    t.index %w[actionable_type actionable_id], name: 'index_actions_on_actionable_type_and_actionable_id', length: { actionable_type: 191 }
-    t.index ['bot_id'], name: 'index_actions_on_bot_id'
+ActiveRecord::Schema.define(version: 2018_07_08_211037) do
+
+  create_table "actions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+    t.bigint "bot_id"
+    t.string "actionable_type"
+    t.bigint "actionable_id"
+    t.index ["actionable_type", "actionable_id"], name: "index_actions_on_actionable_type_and_actionable_id", length: { actionable_type: 191 }
+    t.index ["bot_id"], name: "index_actions_on_bot_id"
   end
 
-  create_table 'bot_hashtags', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci', force: :cascade do |t|
-    t.bigint 'bot_id'
-    t.bigint 'hashtag_id'
-    t.text 'notes'
-    t.index ['bot_id'], name: 'index_bot_hashtags_on_bot_id'
-    t.index ['hashtag_id'], name: 'index_bot_hashtags_on_hashtag_id'
+  create_table "bot_hashtags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+    t.bigint "bot_id"
+    t.bigint "hashtag_id"
+    t.text "notes"
+    t.index ["bot_id"], name: "index_bot_hashtags_on_bot_id"
+    t.index ["hashtag_id"], name: "index_bot_hashtags_on_hashtag_id"
   end
 
-  create_table 'bot_types', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci', force: :cascade do |t|
-    t.integer 'severity'
-    t.text 'name'
+  create_table "bot_types", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+    t.integer "severity"
+    t.text "name"
   end
 
-  create_table 'bots', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci', force: :cascade do |t|
-    t.boolean 'suspended'
-    t.boolean 'watched'
-    t.boolean 'success'
-    t.text 'username'
-    t.text 'notes'
-    t.bigint 'type_id'
-    t.index ['type_id'], name: 'index_bots_on_type_id'
+  create_table "bots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+    t.boolean "suspended"
+    t.boolean "watched"
+    t.boolean "success"
+    t.text "username"
+    t.text "notes"
+    t.bigint "type_id"
+    t.index ["type_id"], name: "index_bots_on_type_id"
   end
 
-  create_table 'hashtags', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci', force: :cascade do |t|
-    t.text 'name'
-    t.text 'notes'
+  create_table "hashtags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+    t.text "name"
+    t.text "notes"
   end
 
-  create_table 'investigations', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci', force: :cascade do |t|
+  create_table "investigations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
   end
 
-  create_table 'network_memberships', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci', force: :cascade do |t|
-    t.bigint 'bot_id'
-    t.bigint 'network_id'
-    t.text 'notes'
-    t.index ['bot_id'], name: 'index_network_memberships_on_bot_id'
-    t.index ['network_id'], name: 'index_network_memberships_on_network_id'
+  create_table "network_memberships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+    t.bigint "bot_id"
+    t.bigint "network_id"
+    t.text "notes"
+    t.index ["bot_id"], name: "index_network_memberships_on_bot_id"
+    t.index ["network_id"], name: "index_network_memberships_on_network_id"
   end
 
-  create_table 'networks', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci', force: :cascade do |t|
-    t.text 'name'
-    t.text 'notes'
+  create_table "networks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+    t.text "name"
+    t.text "notes"
   end
 
-  create_table 'reports', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci', force: :cascade do |t|
+  create_table "reports", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
   end
 
-  create_table 'status_checks', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci', force: :cascade do |t|
+  create_table "roles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["name"], name: "index_roles_on_name"
+    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
   end
+
+  create_table "status_checks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "users_roles", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "role_id"
+    t.index ["role_id"], name: "index_users_roles_on_role_id"
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
+    t.index ["user_id"], name: "index_users_roles_on_user_id"
+  end
+
 end
