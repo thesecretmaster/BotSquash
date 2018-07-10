25d1c542f2bfe7dd4a2e267439102c7989cc8ee5
ArtOfCode- <hello@artofcode.co.uk>
2018-07-10 18:00:18 +0100

    JS cleanup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,83 +3,76 @@ import '../form_polyfill.js';
 import '../twitter.js';
 
 $(document).on('show.bs.modal', '#editBotModal', function(e) {
-  var ele = $(e.relatedTarget);
+  const ele = $(e.relatedTarget);
   $("#editBotModalForm")[0].reset();
-  var username = ele.data('username');
-  var type_id = ele.data('type-id');
-  var bot_id = ele.data('bot-id');
+  const username = ele.data('username');
+  const type_id = ele.data('type-id');
+  const bot_id = ele.data('bot-id');
   $("#editBotModal form input[name=bot\\[username\\]]").val(username);
   $("#editBotModal form select[name=bot\\[type_id\\]]").val(type_id);
   $("#editBotModal form input[name=id]").val(bot_id);
 });
 
-$(document).on('ajax:success', "#editBotModalForm", function(e, data, status, xhr){
-  var username = $("#editBotModal form input[name=bot\\[username\\]]").val();
-  var id = $("#editBotModal form input[name=id]").val();
-  var span = $("td:nth-child(1) span[data-bot-id="+id+"]");
+$(document).on('ajax:success', "#editBotModalForm", function(e, data){
+  const username = $("#editBotModal form input[name=bot\\[username\\]]").val();
+  const id = $("#editBotModal form input[name=id]").val();
+  const span = $("td:nth-child(1) span[data-bot-id="+id+"]");
   span.text(data.username);
   span.removeClass(/(text-\w+)/i.exec(span[0].className)[1]).addClass(data.color_class);
   $("#editBotModal").modal('hide');
 });
 
 $(document).on('show.bs.modal', '#botInfoModal', function(e) {
-  var ele = $(e.relatedTarget);
-  var username = ele.data('username');
-  var bot_id = ele.data('bot-id');
-  var twitter_username = ele.data('twttr');
+  const ele = $(e.relatedTarget);
+  const username = ele.data('username');
+  const bot_id = ele.data('bot-id');
+  const twitter_username = ele.data('twttr');
   $("#botInfoModal .username").text(username);
   // TODO: Fetch further bot info async
-  twttr.widgets.createTimeline(
-    {
-      sourceType: "profile",
-      screenName: twitter_username
-    },
-    document.getElementById("botInfoModal").getElementsByClassName("tweet-container")[0]
-  );
-})
+  twttr.widgets.createTimeline({
+    sourceType: "profile",
+    screenName: twitter_username
+  }, document.getElementById("botInfoModal").getElementsByClassName("tweet-container")[0]);
+});
 
 $(document).on('show.bs.modal', '#newHashtagModal', function(e) {
-  var ele = $(e.relatedTarget);
+  const ele = $(e.relatedTarget);
   $("#newHashtagModalForm")[0].reset();
-  var bot_id = ele.data('bot-id');
-  console.log(bot_id);
+  const bot_id = ele.data('bot-id');
   $("#newHashtagModal form input[name=bot\\[id\\]]").val(bot_id);
 });
 
 $(document).on('ajax:success', "#newHashtagModalForm", function(e, data, status, xhr){
-  var newEle = $.parseHTML(data.elements);
+  const newEle = $.parseHTML(data.elements);
   $("td:nth-child(2) span.id-list").append(newEle);
   $("#newHashtagModal").modal('hide');
 });
 
 $(document).on('show.bs.modal', '#newNetworkMembershipModal', function(e) {
-  var ele = $(e.relatedTarget);
+  const ele = $(e.relatedTarget);
   $("#newNetworkMembershipModalForm")[0].reset();
-  var bot_id = ele.data('bot-id');
-  console.log(bot_id);
+  const bot_id = ele.data('bot-id');
   $("#newNetworkMembershipModal form input[name=bot\\[id\\]]").val(bot_id);
 });
 
-$(document).on('ajax:success', "#newNetworkMembershipModalForm", function(e, data, status, xhr){
-  var newEle = $.parseHTML(data.elements);
+$(document).on('ajax:success', "#newNetworkMembershipModalForm", function(e, data){
+  const newEle = $.parseHTML(data.elements);
   $("td:nth-child(3) span.id-list").append(newEle);
   $("#newNetworkMembershipModal").modal('hide');
 });
 
 $(document).on('show.bs.modal', '#hashtagModal', function(e) {
-  var ele = $(e.relatedTarget);
-  console.log($("#hashtagModal .id"));
-  var id = ele.data('id');
-  var name = ele.data('name');
+  const ele = $(e.relatedTarget);
+  const id = ele.data('id');
+  const name = ele.data('name');
   $("#hashtagModal span.id").text(id);
   $("#hashtagModal span.name").text(name);
 });
 
 $(document).on('show.bs.modal', '#networkModal', function(e) {
-  var ele = $(e.relatedTarget);
-  console.log(ele);
-  var id = ele.data('id');
-  var name = ele.data('name');
+  const ele = $(e.relatedTarget);
+  const id = ele.data('id');
+  const name = ele.data('name');
   $("#networkModal span.id").text(id);
   $("#networkModal span.name").text(name);
 });
