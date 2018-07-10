31fcb010c0a390acb7cafbcff64c0dc599bea9ba
ArtOfCode- <hello@artofcode.co.uk>
2018-07-10 13:53:11 +0100

    Import UJS drivers

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,32 +1,21 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
-
-console.log('Hello World from Webpacker 1')
+import {} from 'jquery-ujs';
 
 window.twttr = (function(d, s, id) {
-var js, fjs = d.getElementsByTagName(s)[0],
-t = window.twttr || {};
-if (d.getElementById(id)) return t;
-js = d.createElement(s);
-js.id = id;
-js.src = "https://platform.twitter.com/widgets.js";
-fjs.parentNode.insertBefore(js, fjs);
+  var js, fjs = d.getElementsByTagName(s)[0],
+  t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
 
-t._e = [];
-t.ready = function(f) {
-t._e.push(f);
-};
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
 
-return t;
+  return t;
 }(document, "script", "twitter-wjs"));
-
-console.log("Twitter loaded");
 
 $(document).on('show.bs.modal', '#editBotModal', function(e) {
   var ele = $(e.relatedTarget);
