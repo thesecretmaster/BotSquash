b966817b2acb905be5fb66072e45440b1c5f3332
thesecretmaster <thesecretmaster@developingtechnician.com>
2018-07-10 10:13:10 -0400

    Added form polyfill and cleaned up JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,21 +1,6 @@
 import {} from 'jquery-ujs';
-
-window.twttr = (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0],
-  t = window.twttr || {};
-  if (d.getElementById(id)) return t;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://platform.twitter.com/widgets.js";
-  fjs.parentNode.insertBefore(js, fjs);
-
-  t._e = [];
-  t.ready = function(f) {
-    t._e.push(f);
-  };
-
-  return t;
-}(document, "script", "twitter-wjs"));
+import '../form_polyfill.js';
+import '../twitter.js';
 
 $(document).on('show.bs.modal', '#editBotModal', function(e) {
   var ele = $(e.relatedTarget);
